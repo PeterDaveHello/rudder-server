1fc7748f856a09dc67b9b428334090e534d19732
Francesco Casula <fra.casula@gmail.com>
2023-08-25 17:07:03 +0200

    chore: struct approach

--- warehouse/logfield/logfield.go
+++ warehouse/logfield/logfield.go
@@ -6,60 +6,71 @@ import (
 	"github.com/rudderlabs/rudder-go-kit/logger"
 )
 
-const (
-	UploadJobID                = "uploadJobID"
-	UploadStatus               = "uploadStatus"
-	UseRudderStorage           = "useRudderStorage"
-	SourceType                 = "sourceType"
-	DestinationType            = "destinationType"
-	DestinationRevisionID      = "destinationRevisionID"
-	DestinationValidationsStep = "step"
-	WorkspaceID                = "workspaceID"
-	Namespace                  = "namespace"
-	Schema                     = "schema"
-	ColumnName                 = "columnName"
-	ColumnType                 = "columnType"
-	Priority                   = "priority"
-	Retried                    = "retried"
-	Attempt                    = "attempt"
-	LoadFileType               = "loadFileType"
-	ErrorMapping               = "errorMapping"
-	DestinationCredsValid      = "destinationCredsValid"
-	QueryExecutionTime         = "queryExecutionTime"
-	StagingTableName           = "stagingTableName"
-)
+//const (
+//	UploadJobID                = "uploadJobID"
+//	UploadStatus               = "uploadStatus"
+//	UseRudderStorage           = "useRudderStorage"
+//	SourceType                 = "sourceType"
+//	DestinationType            = "destinationType"
+//	DestinationRevisionID      = "destinationRevisionID"
+//	DestinationValidationsStep = "step"
+//	WorkspaceID                = "workspaceID"
+//	Namespace                  = "namespace"
+//	Schema                     = "schema"
+//	ColumnName                 = "columnName"
+//	ColumnType                 = "columnType"
+//	Priority                   = "priority"
+//	Retried                    = "retried"
+//	Attempt                    = "attempt"
+//	LoadFileType               = "loadFileType"
+//	ErrorMapping               = "errorMapping"
+//	DestinationCredsValid      = "destinationCredsValid"
+//	QueryExecutionTime         = "queryExecutionTime"
+//	StagingTableName           = "stagingTableName"
+//)
 
-// TODO finish converting constants to functions to experiment
-
-type Field func(v interface{}) KeyValue
-
-type KeyValue struct {
-	k string
-	v interface{}
+type Fields struct {
+	SourceType            string
+	DestinationType       string
+	DestinationRevisionID string
+	WorkspaceID           string
+	Namespace             string
+	Warehouse             struct {
+		UploadJobID      string
+		UploadStatus     string
+		UseRudderStorage string
+	}
+	// TODO add more fields here
 }
 
-var (
-	UploadID      Field = func(v interface{}) KeyValue { return KeyValue{"uploadID", v} }
-	SourceID      Field = func(v interface{}) KeyValue { return KeyValue{"sourceID", v} }
-	DestinationID Field = func(v interface{}) KeyValue { return KeyValue{"destinationID", v} }
-	TableName     Field = func(v interface{}) KeyValue { return KeyValue{"tableName", v} }
-	Location      Field = func(v interface{}) KeyValue { return KeyValue{"location", v} }
-	Query         Field = func(v interface{}) KeyValue { return KeyValue{"query", v} }
-	Error         Field = func(v interface{}) KeyValue {
-		if err, ok := v.(error); ok {
-			return KeyValue{"error", err.Error()}
-		} else {
-			return KeyValue{"error", v}
-		}
+func (f *Fields) keyAndValues() []interface{} {
+	var kvs []interface{}
+	if f.SourceType != "" {
+		kvs = append(kvs, "sourceType", f.SourceType)
 	}
-
-	// Generic should be used only when there is no other option and it doesn't make sense to create a new Field
-	Generic = func(k string) Field {
-		return func(v interface{}) KeyValue {
-			return KeyValue{k, v}
-		}
+	if f.DestinationType != "" {
+		kvs = append(kvs, "destinationType", f.DestinationType)
 	}
-)
+	if f.DestinationRevisionID != "" {
+		kvs = append(kvs, "destinationRevisionID", f.DestinationRevisionID)
+	}
+	if f.WorkspaceID != "" {
+		kvs = append(kvs, "workspaceID", f.WorkspaceID)
+	}
+	if f.Namespace != "" {
+		kvs = append(kvs, "namespace", f.Namespace)
+	}
+	if f.Warehouse.UploadJobID != "" {
+		kvs = append(kvs, "uploadJobID", f.Warehouse.UploadJobID)
+	}
+	if f.Warehouse.UploadStatus != "" {
+		kvs = append(kvs, "uploadStatus", f.Warehouse.UploadStatus)
+	}
+	if f.Warehouse.UseRudderStorage != "" {
+		kvs = append(kvs, "useRudderStorage", f.Warehouse.UseRudderStorage)
+	}
+	return kvs
+}
 
 func NewLogger(component string, l logger.Logger) Logger {
 	return Logger{component: component, logger: l}
@@ -79,42 +90,27 @@ func (l *Logger) init() {
 	})
 }
 
-func (l *Logger) Infow(msg string, kvs ...KeyValue) {
+func (l *Logger) Infow(msg string, fields Fields) {
 	l.init()
-	l.logger.Infow(msg, l.keyAndValues(kvs)...)
+	l.logger.Infow(msg, fields)
 }
 
-func (l *Logger) Debugw(msg string, kvs ...KeyValue) {
+func (l *Logger) Debugw(msg string, fields Fields) {
 	l.init()
-	l.logger.Debugw(msg, l.keyAndValues(kvs)...)
+	l.logger.Debugw(msg, fields)
 }
 
-func (l *Logger) Errorw(msg string, kvs ...KeyValue) {
+func (l *Logger) Errorw(msg string, fields Fields) {
 	l.init()
-	l.logger.Errorw(msg, l.keyAndValues(kvs)...)
+	l.logger.Errorw(msg, fields)
 }
 
-func (l *Logger) Warnw(msg string, kvs ...KeyValue) {
+func (l *Logger) Warnw(msg string, fields Fields) {
 	l.init()
-	l.logger.Warnw(msg, l.keyAndValues(kvs)...)
+	l.logger.Warnw(msg, fields)
 }
 
-func (l *Logger) Fatalw(msg string, kvs ...KeyValue) {
+func (l *Logger) Fatalw(msg string, fields Fields) {
 	l.init()
-	l.logger.Fatalw(msg, l.keyAndValues(kvs)...)
-}
-
-func (l *Logger) keyAndValues(kvs []KeyValue) []interface{} {
-	length := len(kvs) * 2
-	if l.component != "" {
-		length += 2
-	}
-	s := make([]interface{}, 0, length)
-	for _, kv := range kvs {
-		s = append(s, kv.k, kv.v)
-	}
-	if l.component != "" {
-		s = append(s, "component", l.component)
-	}
-	return s
+	l.logger.Fatalw(msg, fields)
 }
